da81f4ee69d2fe05ae3f0356e40f7470797de54c
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-15 16:54:11 -0600

    canvas config

--- js/game.js
+++ js/game.js
@@ -1,1 +1,9 @@
-console.log(maps);
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load", startGame);
+
+function startGame() {
+  //   game.fillRect(0, 0, 100, 100);
+  //   game.clearRect(0, 0, 50, 50);
+}
