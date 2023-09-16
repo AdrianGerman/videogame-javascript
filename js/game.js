46bed229236922a42d0364c08a937c68268ce6a7
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-15 18:04:06 -0600

    windows bomb

--- js/game.js
+++ js/game.js
@@ -4,6 +4,30 @@ const game = canvas.getContext("2d");
 window.addEventListener("load", startGame);
 
 function startGame() {
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  const elementsSize = canvasSize / 10 - 1;
+
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "";
+
+  for (let i = 0; i <= 9; i++) {
+    for (let z = 1; z < 11; z++) {
+      game.fillText(emojis["X"], elementsSize * i, elementsSize * z);
+    }
+  }
+
   //   game.fillRect(0, 0, 100, 100);
   //   game.clearRect(0, 0, 50, 50);
 }
