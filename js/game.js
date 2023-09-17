d81fd03c495179921a6eb22dd6f05a287887429a
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-17 11:29:05 -0600

    button event

--- js/game.js
+++ js/game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
@@ -30,7 +34,7 @@ function startGame() {
 
   const map = maps[0];
   const mapRows = map.trim().split("\n");
-  const mapRowsCols = mapRows.map(row => row.trim().split(""));
+  const mapRowsCols = mapRows.map((row) => row.trim().split(""));
   console.log({ map, mapRows, mapRowsCols });
 
   mapRowsCols.forEach((row, rowI) => {
@@ -48,4 +52,38 @@ function startGame() {
   //     game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementsSize * (col - 1 / 2), elementsSize * (row - 1 / 2));
   //   }
   // }
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(event) {
+  if (event.key == "ArrowUp") {
+    moveUp();
+  } else if (event.key == "ArrowLeft") {
+    moveLeft();
+  } else if (event.key == "ArrowRight") {
+    moveRight();
+  } else if (event.key == "ArrowDown") {
+    moveDown();
+  }
+}
+
+function moveUp() {
+  console.log("Me quiero mover hacia arriba");
+}
+
+function moveLeft() {
+  console.log("Me quiero mover hacia la izquierda");
+}
+
+function moveRight() {
+  console.log("Me quiero mover hacia la derecha");
+}
+
+function moveDown() {
+  console.log("Me quiero mover hacia abajo");
 }
