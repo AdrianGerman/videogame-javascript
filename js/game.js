edfbcdf3f8172275e2b9eb096c2df3caf8398166
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-18 16:00:15 -0600

    enemy collision

--- js/game.js
+++ js/game.js
@@ -18,6 +18,8 @@ const giftPosition = {
   y: undefined,
 };
 
+let enemyPosition = [];
+
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
@@ -47,6 +49,7 @@ function startGame() {
   const mapRowsCols = mapRows.map((row) => row.trim().split(""));
   // console.log({ map, mapRows, mapRowsCols });
 
+  enemyPosition = [];
   game.clearRect(0, 0, canvasSize, canvasSize);
 
   mapRowsCols.forEach((row, rowI) => {
@@ -64,6 +67,11 @@ function startGame() {
       } else if (col == "I") {
         giftPosition.x = posX;
         giftPosition.y = posY;
+      } else if (col == "X") {
+        enemyPosition.push({
+          x: posX,
+          y: posY,
+        });
       }
 
       game.fillText(emoji, posX, posY);
@@ -89,6 +97,17 @@ function movePlayer() {
   if (giftCollision) {
     console.log("Subiste de nivel");
   }
+
+  const enemyCollision = enemyPosition.find((enemy) => {
+    const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+    const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+    return enemyCollisionX && enemyCollisionY;
+  });
+
+  if (enemyCollision) {
+    console.log("Chocaste contra un enemigo!!!");
+  }
+
   game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
 }
 
